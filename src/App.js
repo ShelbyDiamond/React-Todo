de7c471af940364ac9744d48d94547bb949d434e
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-21 13:08:12 -0700

    updated styling for portfolio

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import TodoForm from './components/TodoComponents/TodoForm';
-import TodoList from './components/TodoComponents/TodoList';
-import styled from 'styled-components';
+import React from "react"
+import TodoForm from "./components/TodoComponents/TodoForm"
+import TodoList from "./components/TodoComponents/TodoList"
+import styled from "styled-components"
 
 const Background = styled.div`
-background: lightpink;
+  background: lightpink;
 `
 const toDoList = [
   {
-    task: 'Walk the fish',
+    task: "Walk the fish",
     id: 1528817077286,
     completed: false
   },
 
   {
-    task: 'Feed the car',
+    task: "Feed the car",
     id: 1528817084358,
     completed: false
   },
 
   {
-    task: 'Paint with Russell',
+    task: "Paint with Russell",
     id: 2282017222817,
     completed: false
   },
   {
-    task: 'Clean the soap',
-    id: '0322201942579',
+    task: "Clean the soap",
+    id: 0322201942579,
     completed: false
   }
 ]
@@ -35,12 +35,12 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  constructor(){
-    super();
-    this.state ={
+  constructor() {
+    super()
+    this.state = {
       // myList: toDoList
       myList: []
-     }
+    }
   }
 
   toggleTask = id => {
@@ -50,15 +50,15 @@ class App extends React.Component {
           return {
             ...chore,
             completed: !chore.completed
-          };
+          }
         } else {
-          return chore;
+          return chore
         }
       })
     })
   }
 
-  addTask = taskName =>{
+  addTask = taskName => {
     const newTask = {
       task: taskName,
       id: Date.now(),
@@ -68,26 +68,27 @@ class App extends React.Component {
       myList: [...this.state.myList, newTask]
     })
   }
-      clearCompleted = () => {
-        this.setState({
-          myList: this.state.myList.filter(listItem => !listItem.completed)
-        });
-      }
+  clearCompleted = () => {
+    this.setState({
+      myList: this.state.myList.filter(listItem => !listItem.completed)
+    })
+  }
 
   render() {
     console.log(this.state.myList)
     return (
       <Background>
         <div className="App">
-          <div className="header">        <h2>Hello, your Majesty!</h2>
-          <p>What tasks can I complete for you today</p>
-          <TodoForm addTask={this.addTask} />
-          <TodoList myList={this.state.myList} toggleTask={this.toggleTask} />
+          <div className="header">
+            <h2>Hello, your Majesty!</h2>
+            <p>What tasks can I complete for you today</p>
+            <TodoForm addTask={this.addTask} />
+            <TodoList myList={this.state.myList} toggleTask={this.toggleTask} />
           </div>
         </div>
       </Background>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react"
 
 const Todo = props => {
   return (
     <div
-      className={`chore${props.chore.completed ? 'completed' : ""}`}
-      onClick={() => props.toggleTask(props.chore.id)}>
+      className={`chore${props.chore.completed ? "completed" : ""}`}
+      onClick={() => props.toggleTask(props.chore.id)}
+    >
       <p>{props.chore.task}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Todo;
+export default Todo
